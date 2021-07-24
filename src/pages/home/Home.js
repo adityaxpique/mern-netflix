d9445c5d6c54d9ae0950041e30e3c8a669121b15
aditya <aditya29ks@gmail.com>
2021-07-24 23:09:47 +0530

    listttt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'  
 import Featured from '../../components/featured/Featured';
 import Navbar from "../../components/navbar/Navbar";
+import List from "../../components/list/List";
 import "./home.scss";
 
 
@@ -9,6 +10,10 @@ const Home = () => {
     <div className="home">
       <Navbar />
       <Featured/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   );
 };
